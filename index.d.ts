1a75a48941944b5026222f6b27501c0aeaa97c98
Harry Solovay <harrysolovay@gmail.com>
2019-03-09 21:17:06 -0500

    tests and types

---
+++ index.d.ts
@@ -0,0 +1,2 @@
+export function isHook(inQuestion: any): boolean
+export function isHookName(inQuestion: string): boolean
--- test.js
+++ test.js
@@ -1,1 +1,19 @@
-const {isHook, isHookName} from './build'
+import test from 'ava'
+import {isHook, isHookName} from './build'
+import {useEffect, useMemo, useState, useContext} from 'react'
+
+test('minimal run-through', t => {
+  t.plan(10)
+
+  t.is(isHook(useEffect), true)
+  t.is(isHook(useMemo), true)
+  t.is(isHook(useState), true)
+  t.is(isHook(useContext), true)
+  t.is(isHook(() => {}), false)
+
+  t.is(isHookName('useEffect'), true)
+  t.is(isHookName('useMemo'), true)
+  t.is(isHookName('useState'), true)
+  t.is(isHookName('useContext'), true)
+  t.is(isHookName('nope'), false)
+})
